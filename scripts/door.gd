9f341107042e9126b0a31d6b964091d8dab17b7b
brennosouza <brenno.souzarj@gmail.com>
2021-08-22 15:03:56 -0300

    Basic door
    * Simple door that rotates away from the player

---
+++ scripts/door.gd
@@ -0,0 +1,27 @@
+extends Spatial
+
+
+onready var player = get_tree().get_nodes_in_group("player")[0]
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("interact"):
+		
+		var direction = self.global_transform.origin.direction_to(player.global_transform.origin)
+		print_debug(direction)
+		
+		if direction.z < 0:
+			rotate_y(-90)
+		else:
+			rotate_y(90)
+		
+
+func interact(player):
+	pass
+	
